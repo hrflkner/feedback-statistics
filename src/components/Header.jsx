489c30d7047de3274f739f4bf6ac37085bf0275d
hrflkner <hunter.wm.faulkner@gmail.com>
2022-05-05 00:47:25 +1000

    Lesson 12 & 13 Code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,29 @@
-function Header() {
-    return <div>Header</div>;
+import PropTypes from 'prop-types';
+function Header({ text, bgColor, textColor }) {
+    const headerStyles = {
+        backgroundColor: bgColor,
+        color: textColor,
+    };
+
+    return (
+        <header style={headerStyles}>
+            <div className="">
+                <h2>{text}</h2>
+            </div>
+        </header>
+    );
 }
+
+Header.defaultProps = {
+    text: 'Feedback Statistics',
+    bgColor: 'rgba(0,0,0,0.4)',
+    textColor: '#ff6a95',
+};
+
+Header.propTypes = {
+    text: PropTypes.string,
+    bgColor: PropTypes.string,
+    textColor: PropTypes.string,
+};
 
 export default Header;
