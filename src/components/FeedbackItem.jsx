eab92e0f4685d7b75822e69de699623e001945ee
hrflkner <hunter.wm.faulkner@gmail.com>
2022-06-17 11:36:55 +1000

    Add Edit Button

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,4 +1,4 @@
-import { FaTimes } from 'react-icons/fa';
+import { FaTimes, FaEdit } from 'react-icons/fa';
 import PropTypes from 'prop-types';
 import Card from '../shared/Card';
 
@@ -9,6 +9,9 @@ function FeedbackItem({ item, handleDelete }) {
             <div className="num-display">{item.rating}</div>
             <button onClick={() => handleDelete(item.id)} className="close">
                 <FaTimes color="purple" />
+            </button>
+            <button className="edit">
+                <FaEdit color="purple" />
             </button>
             <div className="text-display">{item.text}</div>
         </Card>
